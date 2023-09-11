79d5ebab629c0ca9d5a0422381d45a78288f9ffb
Kenneth Pauletti <kennethpauletti@gmail.com>
2023-09-11 17:12:44 +0000

    gh actions init

---
+++ apps/infra/.eslintrc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    env: {
+        es6: true,
+        node: true
+    },
+  };
